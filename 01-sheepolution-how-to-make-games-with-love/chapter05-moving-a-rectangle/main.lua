019732ac3b860b57cdab41e080db3aba4c3d71dc
Rico Moorman <rico.moorman@gmail.com>
2018-06-12 09:16:57 +0200

    Make the rectangle move

--- 01-sheepolution-how-to-make-games-with-love/chapter05-moving-a-rectangle/main.lua
+++ 01-sheepolution-how-to-make-games-with-love/chapter05-moving-a-rectangle/main.lua
@@ -1,9 +1,11 @@
 function love.load()
+  x = 100
 end
 
 function love.update()
+  x = x + 5
 end
 
 function love.draw()
-  love.graphics.rectangle("line", 100, 50, 200, 150)
+  love.graphics.rectangle("line", x, 50, 200, 150)
 end
