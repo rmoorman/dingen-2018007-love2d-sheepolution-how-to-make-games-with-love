c522b0803be42a347a526c00eae6656ac3c8a958
Rico Moorman <rico.moorman@gmail.com>
2018-06-12 09:48:58 +0200

    Move based on arrow keys

--- 01-sheepolution-how-to-make-games-with-love/chapter06-if-statements/main.lua
+++ 01-sheepolution-how-to-make-games-with-love/chapter06-if-statements/main.lua
@@ -1,18 +1,20 @@
 function love.load()
   x = 100
-  move = true
+  y = 50
 end
 
 function love.update(dt)
-  if x > 500 then
-    move = false
-  end
-
-  if move then
+  if love.keyboard.isDown("right") then
     x = x + 100 * dt
+  elseif love.keyboard.isDown("left") then
+    x = x - 100 * dt
+  elseif love.keyboard.isDown("down") then
+    y = y + 100 * dt
+  elseif love.keyboard.isDown("up") then
+    y = y - 100 * dt
   end
 end
 
 function love.draw()
-  love.graphics.rectangle("line", x, 50, 200, 150)
+  love.graphics.rectangle("line", x, y, 200, 150)
 end
