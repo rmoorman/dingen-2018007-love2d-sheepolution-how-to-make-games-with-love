ad5aec643b1b30f1befb6f739f126681c5c3d08d
Rico Moorman <rico.moorman@gmail.com>
2018-06-19 23:40:25 +0200

    Add enemy with movement

--- 01-sheepolution-how-to-make-games-with-love/chapter14-game-shoot-the-enemy/main.lua
+++ 01-sheepolution-how-to-make-games-with-love/chapter14-game-shoot-the-enemy/main.lua
@@ -1,14 +1,18 @@
 Object = require("classic")
 require("player")
+require("enemy")
 
 function love.load()
   player = Player()
+  enemy = Enemy()
 end
 
 function love.update(dt)
   player:update(dt)
+  enemy:update(dt)
 end
 
 function love.draw()
   player:draw()
+  enemy:draw()
 end
