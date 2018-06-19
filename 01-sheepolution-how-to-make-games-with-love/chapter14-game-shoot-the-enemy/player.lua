cb785fb1b8122b792e30f51ebfdffbb6600b21b2
Rico Moorman <rico.moorman@gmail.com>
2018-06-19 23:26:38 +0200

    Make player move

--- 01-sheepolution-how-to-make-games-with-love/chapter14-game-shoot-the-enemy/player.lua
+++ 01-sheepolution-how-to-make-games-with-love/chapter14-game-shoot-the-enemy/player.lua
@@ -2,11 +2,20 @@ Player = Object:extend()
 
 function Player:new()
   self.image = love.graphics.newImage("media/panda.png")
+  self.x = 300
+  self.y = 20
+  self.speed = 500
 end
 
-function Player.update(dt)
+function Player:update(dt)
+  if love.keyboard.isDown("left") then
+    self.x = self.x - self.speed * dt
+  end
+  if love.keyboard.isDown("right") then
+    self.x = self.x + self.speed * dt
+  end
 end
 
 function Player:draw()
-  love.graphics.draw(self.image)
+  love.graphics.draw(self.image, self.x, self.y)
 end
